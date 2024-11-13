b451f0bc569f2d1e678d800109eb98db09ea00c8
Michał Okulewicz <mokulewicz@betacom.com.pl>
2024-11-13 19:42:46 +0100

    Added vehicles costs

---
+++ VROOM/Models/Cost.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace VROOM
+{
+    public class Cost
+    {
+        [JsonProperty("fixed")]
+        /// <summary>
+        /// Integer defining the cost of using this vehicle in the solution (defaults to 0).
+        /// </summary>
+        public int Fixed { get; set; } = 0;
+
+        [JsonProperty("per_hour")]
+        /// <summary>
+        /// Integer defining the cost for one hour of travel time with this vehicle (defaults to 3600).
+        /// </summary>
+        public int PerHour { get; set; } = 3600;
+
+        [JsonProperty("per_km")]
+        /// <summary>
+        /// Integer defining the cost for one km of travel time with this vehicle (defaults to 0).
+        /// </summary>
+        public int PerKm { get; set; } = 0;
+    }
+}
