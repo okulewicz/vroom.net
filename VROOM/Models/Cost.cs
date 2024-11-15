4a3545893eb6792311722a1313a6e8fd433b90a3
Michał Okulewicz <mokulewicz@betacom.com.pl>
2024-11-15 01:20:30 +0100

    make some costs nullable

--- VROOM/Models/Cost.cs
+++ VROOM/Models/Cost.cs
@@ -8,18 +8,18 @@ namespace VROOM
         /// <summary>
         /// Integer defining the cost of using this vehicle in the solution (defaults to 0).
         /// </summary>
-        public int Fixed { get; set; } = 0;
+        public int? Fixed { get; set; }
 
         [JsonProperty("per_hour")]
         /// <summary>
         /// Integer defining the cost for one hour of travel time with this vehicle (defaults to 3600).
         /// </summary>
-        public int PerHour { get; set; } = 3600;
+        public int? PerHour { get; set; }
 
         [JsonProperty("per_km")]
         /// <summary>
         /// Integer defining the cost for one km of travel time with this vehicle (defaults to 0).
         /// </summary>
-        public int PerKm { get; set; } = 0;
+        public int? PerKm { get; set; }
     }
 }
